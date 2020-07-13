11d153dfaf99deb4a824bfa7c5ea7bc84f92012d
Atsushi Watanabe <atsushi.w@ieee.org>
2020-07-13 14:37:03 +0900

    Fix session ID generation
    Use crypto random according to
    https://tools.ietf.org/html/draft-ietf-rtcweb-jsep-26#section-5.2.1

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pion/sdp/v2
 
 go 1.12
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/pion/randutil v0.1.0
+	github.com/stretchr/testify v1.6.1
+)
--- jsep.go
+++ jsep.go
@@ -50,12 +50,20 @@ var extMapURI = map[int]string{
 
 // NewJSEPSessionDescription creates a new SessionDescription with
 // some settings that are required by the JSEP spec.
-func NewJSEPSessionDescription(identity bool) *SessionDescription {
+//
+// Note: Since v2.4.0, session ID has been fixed to use crypto random according to
+//       JSEP spec, so that NewJSEPSessionDescription now returns error as a second
+//       return value.
+func NewJSEPSessionDescription(identity bool) (*SessionDescription, error) {
+	sid, err := newSessionID()
+	if err != nil {
+		return nil, err
+	}
 	d := &SessionDescription{
 		Version: 0,
 		Origin: Origin{
 			Username:       "-",
-			SessionID:      newSessionID(),
+			SessionID:      sid,
 			SessionVersion: uint64(time.Now().Unix()),
 			NetworkType:    "IN",
 			AddressType:    "IP4",
@@ -80,7 +88,7 @@ func NewJSEPSessionDescription(identity bool) *SessionDescription {
 		d.WithPropertyAttribute(AttrKeyIdentity)
 	}
 
-	return d
+	return d, nil
 }
 
 // WithPropertyAttribute adds a property attribute 'a=key' to the session description
