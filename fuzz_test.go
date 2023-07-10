46b7f471d058f8683381d2a629cb8b427f3b329f
tanghao <tanghao1996.seu@gmail.com>
2023-07-10 16:33:25 +0000

    Test: port old fuzzing code to go-fuzzing

---
+++ fuzz_test.go
@@ -0,0 +1,20 @@
+package sdp
+
+import "testing"
+
+func FuzzUnmarshal(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte(CanonicalMarshalSDP))
+	f.Fuzz(func(t *testing.T, data []byte) {
+		// Check that unmarshalling any byte slice does not panic.
+		var sd SessionDescription
+		if err := sd.Unmarshal(data); err != nil {
+			return
+		}
+		// Check that we can marshal anything we unmarshalled.
+		_, err := sd.Marshal()
+		if err != nil {
+			t.Fatalf("failed to marshal")
+		}
+	})
+}
